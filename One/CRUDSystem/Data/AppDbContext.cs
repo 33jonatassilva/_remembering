67c1d1e764fdad09123eadd5e887d8beaf400ebb
Jônatas Silva <125514563+33jonatassilva@users.noreply.github.com>
2025-01-27 09:53:39 -0300

    Revert "Career add -"
    This reverts commit 38d2c95c6a290245c3d815a11da31566e3170ce9.
    
    Revertendo o commit 38d2c95c6a290245c3d815a11da31566e3170ce9  para corrigir um erro.

--- One/CRUDSystem/Data/AppDbContext.cs
+++ One/CRUDSystem/Data/AppDbContext.cs
@@ -4,14 +4,9 @@ namespace CRUDSystem.Data
 {
     public class AppDbContext : DbContext
     {
-
-
-        DbSet<Author> Authors {get; set;}
-        DbSet<Carrer> Carrers {get; set;}
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        
+        DbSet<Author> Authors {get; set;}
       
     }
 }
--- One/CRUDSystem/Program.cs
+++ One/CRUDSystem/Program.cs
@@ -2,10 +2,6 @@
 
 using CRUDSystem.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,24 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configurar os serviços
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // Necessário para o Swagger funcionar
-builder.Services.AddSwaggerGen(); // Adiciona o Swagger
-
 var app = builder.Build();
-
-// Configurar o pipeline de requisição
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger(); // Habilita o middleware do Swagger
-    app.UseSwaggerUI(); // Configura a interface do Swagger
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers(); // Mapeia os controllers
 
 app.Run();
